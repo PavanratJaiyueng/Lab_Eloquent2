da46df097cb99c988beebe2c8ac362e6eb08abe8
PavanratJaiyueng <kk0951369719@gmail.com>
2024-01-11 13:50:18 +0700

    first commit

---
+++ app/Models/Salaries.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Salaries extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = ['emp_no', 'from_date'];
+    public $incrementing = false;
+    public function employee(){
+        return $this->belongsTo(Employees::class);
+    }
+}
